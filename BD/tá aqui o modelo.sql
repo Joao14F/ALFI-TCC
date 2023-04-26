90d8def454d7f2aa801bc081793a96b417712ea0
Filipe <ftoledp46@gmail.com>
2023-04-26 13:35:36 -0300

    tarde 26/04

---
+++ BD/tá aqui o modelo.sql
@@ -0,0 +1,12 @@
+create database modelo
+use modelo
+
+create table modelo (
+Título varchar(50) not null,
+Sustentável varchar(50) not null,
+Tipo varchar(50) not null,
+Comprimento varchar(50) null, 
+Quadril varchar(50) null, 
+Cintura varchar(50) null, 
+Gancho varchar(50) null, 
+Arquivo mediumblob not null);

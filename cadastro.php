39ce4bd5b19d4087097690707059438a088385e0
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-05-25 22:25:58 -0300

    erro resolvido

--- cadastro.php
+++ cadastro.php
@@ -1,20 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost','root','','tcc');
-if (mysqli_connect_errno()) {
-    echo "Falha de conexão com o MySQL: " . mysqli_connect_error();
-
-    exit();
-    }
-else {
-    echo "Sucesso de conexão com o MySQL";
-
-if (mysqli_query($conn, $sql)) {
-    echo " Deu certo";
-}
-else {
-    echo " Deu merda";
-}
-
+include_once('conexao.php');
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">

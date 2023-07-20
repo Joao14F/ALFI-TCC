caf9991b89da81e9f0dce8b7be0112da2d807aed
StefanyMR <stefanymr208@gmail.com>
2023-07-20 19:04:24 -0300

    pdep FAQ e algumas outras pequenas mudanças

---
+++ pdep.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Política de Privacidade</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap');
+
+body {
+    background: #1B2029;
+    font-family: "Poppins", sans-serif;
+    color: white;
+    font-weight: 500;
+}
+
+header{
+    display: flex;
+    border-bottom: 2px solid white;
+    border-color: #8880FE;
+}
+h1 {
+    color: #8880FE;
+    font-size: 2rem;
+    margin-bottom: 1-rem;
+    text-align: center;
+    padding-left: 26rem;
+}
+
+h2 {
+    padding: 15px;
+    color: #8880FE;
+}
+
+.pb {
+    padding-bottom: 2rem;
+}
+
+p {
+    padding-left: 20px;
+    color: #8880FE;
+    text-align: justify;
+    padding-right: 20px;
+}
+
+li {
+    color: #8880FE;
+}
+
+i {
+    color: #8880FE;
+    padding-left: 0.5rem;
+    padding-top: 1.5rem;
+    margin-left: 0rem;
+}
+
+    </style>
+</head>
+<body>
+    <header>
+    <a href="index.php"><i class="fa-sharp fa-solid fa-angles-left fa-xl"></i></a>
+
+        <h1>POLÍTICA DE PRIVACIDADE</h1>
+    </header>
+
+    <p>Agradecemos por visitar nosso site. A proteção da sua privacidade é importante para nós. 
+        Esta Política de Privacidade descreve como coletamos, usamos, protegemos e compartilhamos 
+        as informações pessoais dos nossos usuários.</p>
+
+    <h2>1- Informações coletadas</h2>
+
+    <p> Podemos coletar as seguintes informações pessoais quando você interage conosco ou utiliza nossos serviços:</p>
+<ul>
+    <li>Nome, endereço de e-mail e número de telefone, informações de contato
+     fornecidas voluntariamente por você. </li>
+    <li> Dados de acesso e atividade no site, incluindo endereço IP. </li>
+</ul>
+
+<h2>2- Uso das informações </h2>
+
+<p>Utilizamos as informações coletadas para os seguintes propósitos:</p>
+
+<ul>
+<li>Fornecer os serviços solicitados e responder a suas solicitações.</li>
+<li>Personalizar sua experiência no site e oferecer conteúdo relevante.</li>
+<li>Monitorar e analisar o uso do site, para melhorar sua funcionalidade e desempenho.</li>
+</ul>
+
+<h2>3- Seus direitos </h2>
+
+<p>Você tem o direito de acessar, corrigir, atualizar ou excluir suas informações pessoais. 
+Também pode optar por não receber comunicações promocionais futuras. Para exercer esses direitos,
+ entre em contato conosco por meio dos detalhes fornecidos no final desta política.</p>
+
+ <h2>4- Alterações nesta política </h2>
+
+ <p>Reservamo-nos o direito de atualizar ou modificar esta Política de Privacidade periodicamente. 
+    Quaisquer alterações serão publicadas nesta página com a nova data de atualização. 
+    É sua responsabilidade revisar periodicamente esta política para estar ciente de quaisquer mudanças.</p>
+
+<h2>5- Contato </h2>
+
+<p>Se tiver alguma dúvida, preocupação ou solicitação relacionada à privacidade, entre em contato conosco 
+através dos seguintes detalhes:
+</p>
+<p class="pb">
+Nos rodapés do nosso site estão disponíveis o email da equipe no qual você pode nos contatar diretamente e temos um formulário específico para mensagens, sugestões e etc, no qual revisamos e respondemos em um prazo de no máximo uma semana.
+</p>
+
+</body>
+</html>

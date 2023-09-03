c51a852b3da0a050ab158e001b7e14f419fca411
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-09-02 23:26:21 -0300

    update npm

--- paginaçao.php
+++ paginaçao.php
@@ -1,13 +1,13 @@
 <?php
 echo '<div class="row">';
-echo '<div class="pagination">';
+echo '<div class="paginaton">';
 
 if ($pagina_atual > 1) {
     echo '<a href="?pagina=' . ($pagina_atual - 1);
     if (isset($_GET['peça'])) {
         echo '&peça=' . urlencode($_GET['peça']);
     }
-    echo '" class="pagination-link">Anterior</a>' . ' ';
+    echo '" class="paginaton-link">Anterior</a>' . ' ';
 }
 
 for ($i = 1; $i <= $total_paginas; $i++) {
@@ -15,7 +15,7 @@ for ($i = 1; $i <= $total_paginas; $i++) {
     if (isset($_GET['peça'])) {
         echo '&peça=' . urlencode($_GET['peça']);
     }
-    echo '" class="pagination-link">' . $i . '</a>' . ' ';
+    echo '" class="paginaton-link">' . $i . '</a>' . ' ';
 }
 
 if ($pagina_atual < $total_paginas) {
@@ -23,7 +23,7 @@ if ($pagina_atual < $total_paginas) {
     if (isset($_GET['peça'])) {
         echo '&peça=' . urlencode($_GET['peça']);
     }
-    echo '" class="pagination-link">Próxima</a>' . ' ';
+    echo '" class="paginaton-link">Próxima</a>' . ' ';
 }
 
 echo '</div>';

456ab980913324a2998487c6c6f00c7d3f740fa6
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-10-26 13:11:59 -0300

    Feito

--- index.php
+++ index.php
@@ -17,55 +17,67 @@
 
 <body>
     <div class="container-fluid">
-        <div class="row corpo justify-content-center justify-content-md-start">
-            <?php
-            require_once('cabecalho.php');
-            include_once('conexao.php');
-            include('paginaçaoVar.php');
+    <div class="row corpo justify-content-center align-items-center justify-content-md-start">
+    <?php
+    require_once('cabecalho.php');
+    include_once('conexao.php');
+    include('paginaçaoVar.php');
 
-            if (isset($_GET['peça'])) {
-                $peca = $_GET['peça'];
-                if ($peca == 'Toda') {
-                    $query = "SELECT * FROM `modelo` WHERE `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
-                    $stmt = $conn->prepare($query);
-                    $stmt->bind_param("ii", $offset, $resultados_por_pagina);
-                } elseif ($peca == 'Sustentável') {
-                    $query = "SELECT * FROM `modelo` WHERE `Sustentável` = 'sim' And `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
-                    $stmt = $conn->prepare($query);
-                    $stmt->bind_param("ii", $offset, $resultados_por_pagina);
-                } else {
-                    $query = "SELECT * FROM `modelo` WHERE `Tipo` = ? And `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
-                    $stmt = $conn->prepare($query);
-                    $stmt->bind_param("sii", $peca, $offset, $resultados_por_pagina);
-                }
-            } else {
-                $query = "SELECT * FROM `modelo` WHERE `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
-                $stmt = $conn->prepare($query);
-                $stmt->bind_param("ii", $offset, $resultados_por_pagina);
+    if (isset($_GET['peça'])) {
+        $peca = $_GET['peça'];
+        if ($peca == 'Toda') {
+            $query = "SELECT * FROM `modelo` WHERE `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("ii", $offset, $resultados_por_pagina);
+        } elseif ($peca == 'Sustentável') {
+            $query = "SELECT * FROM `modelo` WHERE `Sustentável` = 'sim' And `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("ii", $offset, $resultados_por_pagina);
+        } else {
+            $query = "SELECT * FROM `modelo` WHERE `Tipo` = ? And `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("sii", $peca, $offset, $resultados_por_pagina);
+        }
+    } else {
+        $query = "SELECT * FROM `modelo` WHERE `Verificado` != 'Não' ORDER BY `Id modelo` DESC LIMIT ?, ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ii", $offset, $resultados_por_pagina);
+    }
+    $stmt->execute();
+    $res = $stmt->get_result();
+
+    if ($res && mysqli_num_rows($res) > 0) {
+        // Exibe as imagens dentro do laço `while`
+        while ($row = mysqli_fetch_assoc($res)) {
+            if (isset($row['Capa'])) { // Verifica se a chave 'Capa' está definida
+                $caminho_imagem = $row['Capa'];
+    
+                // Adicione uma classe para facilitar a seleção com JavaScript
+                echo '<div class="col-10 col-md-2 espaço">';
+                echo '<a href="Acesso.php?valor=' . $row['Id modelo'] . '">';
+                echo '<img src="' . $caminho_imagem . '" alt="Imagem" class="modelos w-100 imagem-dinamica" id="imagem' . $row['Id modelo'] . '">';
+                echo '</a>';
+                echo '<p class="text-truncate text-white">' . $row['Título'] . '</p>';
+                echo '</div>';
             }
-            $stmt->execute();
-            $res = $stmt->get_result();
+        }
+    } else {
+        echo '<p class="resultado">Nenhum modelo encontrado</p>';
+    }
+    ?>
+    <script>
+    // Usar JavaScript para obter a largura e definir como o atributo de estilo 'height'
+    document.addEventListener('DOMContentLoaded', function() {
+        var imagens = document.querySelectorAll('.imagem-dinamica');
+        imagens.forEach(function(imagem) {
+            var largura = imagem.clientWidth;
+            imagem.style.height = largura + 'px';
+        });
+    });
+    </script>
+    
+</div>
 
-
-
-            if ($res && mysqli_num_rows($res) > 0) {
-                // Exibe as imagens dentro do laço `while`
-                while ($row = mysqli_fetch_assoc($res)) {
-                    if (isset($row['Capa'])) { // Verifica se a chave 'Capa' está definida
-                        $caminho_imagem = $row['Capa'];
-                        echo '<div class="col-10 col-md-2 espaço">';
-                        echo '<a href="Acesso.php?valor=' . $row['Id modelo'] . '">';
-                        echo '<img src="' . $caminho_imagem . '" alt="Imagem" class="modelos img-fluid">';
-                        echo '</a>';
-                        echo '<p class="text-truncate text-white">' . $row['Título'] . '</p>';
-                        echo '</div>';
-                    }
-                }
-            } else {
-                echo '<p class="resultado">Nenhum modelo encontrado</p>';
-            }
-            ?>
-        </div>
 
         <?php
         require_once('paginaçao.php');

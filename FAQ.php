93c0f9f97749fd1405a9d4f613bd515ed89ea668
StefanyMR <stefanymr208@gmail.com>
2023-10-28 14:39:08 -0300

    pdep e FAQ estilizadas

--- FAQ.php
+++ FAQ.php
@@ -9,199 +9,271 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="icon" type="image/png" href="imagens/logonav.png">
+
     <style>
 
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap');
 
+
+
 body {
     background: #1B2029;
-    font-family: "Poppins", sans-serif;
-    color: white;
-    font-weight: 500;
-    margin: 0;
-    padding: 0;
-}
-
-header{
-    border-bottom: 2px solid white;
-    border-color: #8880FE;
-}
-h1 {
-    color: #8880FE;
-    font-size: 2rem;
-    padding: 15px;
+    text-align: justify;
 }
 
 h2 {
     color: #8880FE;
 }
 
-p {
-    color: #8880FE;
-    text-align: justify;
+.faqs-container {
+  font-family: "Poppins", sans-serif;
+  margin: 0 auto;
+  padding: 25px;
+  color: #f1faee;
+  padding-top: 20px;
+}
+
+.faqs-container .questions-container {
+  box-shadow: 0 4px 8px -3px rgba(0, 0, 0, 0.3);
+}
+
+.faqs-container h2 {
+  padding: 4px 32px;
+  font-size: 28px;
+}
+
+.faqs-container .faq-header {
+  border-radius: 1px;
+  display: flex;
+  background: #8880FE;
+  color: #f1faee;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  padding: 15px;
+  box-shadow: 0 4px 8px -3px rgba(0, 0, 0, 0.3);
+  border-bottom: 1px solid #1B2029;
+}
+
+.faqs-container .faq-header .open,
+.faqs-container .faq-header .close {
+  position: absolute;
+  right: 0;
+  padding: 0 32px;
+  font-size: 22px;
+  font-weight: bold;
+  transform: translateY(-8px);
+  opacity: 0;
+  transition: all 500ms;
+}
+
+.faqs-container .faq-header .open.active,
+.faqs-container .faq-header .close.active {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.faqs-container .faq-header h3 {
+  font-size: 20px;
+  padding: 0 32px;
+}
+
+.faqs-container .content {
+  padding: 0 32px;
+  background: #d3d6d2;
+  line-height: 2;
+  max-height: 0;
+  overflow: hidden;
+  transition: all 500ms;
+  color: #1B2029;
+}
+
+.faqs-container .content.active {
+  max-height: 600px;
 }
 
 img {
-    width: 35%;
-    padding: 0px 0px 15px 0px;
+    width: 18%;
 }
+
 
 .images {
     display: flex;
     justify-content: center;
 }
 
-.images img {
-    max-width: 100%;
-}
+        </style>
 
-@media (max-width: 454px) {
-    h1 {
-        text-align: center;
-    }
-}
-
-@media (min-width: 118px) and (max-width: 767px) {
-    h1 {
-    display: flex;
-    justify-content: center;
-    font-size: 1.5rem;
-    padding: 10px;
-    }
-
-    p {
-        padding: 10px;
-    }
-
-    .p2 {
-        padding: 10px;
-    }
-
-    h2 {
-        padding-left: 10px;
-    }
-
-
-}
-
-@media (min-width: 768px) and (max-width: 1200px) {
-
-    h1 {
-    display: flex;
-    justify-content: center;
-    }
-
-    p {
-        padding: 15px 10px 10px 10px;
-    }
-
-    .p2 {
-        padding:  10px 10px 0px 10px;
-    }
-
-    h2 {
-        padding-left: 10px;
-    }
-
-
-}
-
-@media (min-width: 1201px) and (max-width: 2000px) {
-    h1 {
-    display: flex;
-    justify-content: center;
-    }
-
-    p {
-        padding: 15px 10px 10px 10px;
-    }
-
-    .p2 {
-        padding:  10px 10px 0px 10px;
-    }
-
-    h2 {
-        padding-left: 10px;
-    }
-
-    img {
-    width: 20%;
-    padding: 0px 0px 15px 0px;
-}
-}
-
-
-</style>
 </head>
+
 <body>
-<div class="container-fluid">
 
-<div class="row">
-    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
-     <header>        
-        <h1>Perguntas Feitas Frequentemente</h1>
-     </header>
-    </div>
-</div>
+<div class="faqs-container">
+<div class="row align-items-center">
+            <div class="justify-content-center col-12  col-sm-12 col-md-12 col-lg-12">
+      <h2 class="text-center">Perguntas Feitas Frequentemente</h2>
 
-<div class="row">
-    <div class="col-12"></div>
-        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            
-    <p> Esta é a página de Perguntas Feitas Frequentemente. Aqui estão 
-    esclarecidas as dúvidas mais comuns que surgem sobre o ALFI e suas funcionalidades. As perguntas e suas 
-    respectivas respostas estão separadas por seções, desta forma, procure a área que mais se adequa à sua dúvida,
-    caso não consiga esclarecer suas dúvidas entre em contato conosco para que possamos retornar uma resposta.
+      <p> Esta é a página de Perguntas Feitas Frequentemente. Aqui estão
+    esclarecidas as dúvidas mais comuns que surgem sobre o ALFI e suas funcionalidades. 
+    Caso não consiga esclarecer suas dúvidas entre em contato 
+    conosco para que possamos retornar uma resposta.
     </p>
 
-    <h2>Seção 1 - Cadastrar Modelo</h2>
 
-    <p class="p2">1. A que se referem as medidas do cadastro?</p>
+      <div class="questions-container">
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>1. A que se referem as medidas do cadastro de modelos?</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
 
-    <p class="p2">Resposta: Para ajudar a compreender melhor as áreas das medidas e a que elas se referem temos 
-    está tabela: </p>
-   
-        </div>
-    </div>
+          <div class="content">
+            <p>
+            Para ajudar a compreender melhor as áreas das medidas e a que elas se referem temos
+    está tabela:
 
-    <div class="row">
-        <div class="col-12"></div>
-            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
-               <section class="images">
+            </p>
+
+            <section class="images">
                     <img src="imagens/medidastab.png" alt="TabMedidas">
                </section>
+
+          </div>
         </div>
-    </div>
 
-    <div class="row">
-    <div class="col-12"></div>
-        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>2. O que é sustentabilidade?</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
 
-    <h2>Seção 2 - Sustentabilidade</h2>
+          <div class="content">
+            <p>
+            Sustentabilidade é a capacidade de uso consciente dos recursos naturais sem
+        comprometer o bem-estar das gerações futuras. Seu objetivo principal é encontrar o equilíbrio
+        entre o desenvolvimento econômico e a preservação ambiental.
 
-    <p class="p2">1. O que é sustentabilidade?</p>
+            </p>
+          </div>
+        </div>
 
-    <p class="p2">Resposta: Sustentabilidade é a capacidade de uso consciente dos recursos naturais sem 
-        comprometer o bem-estar das gerações futuras. Seu objetivo principal é encontrar o equilíbrio 
-        entre o desenvolvimento econômico e a preservação ambiental.</p>
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>3. Qual relação a sustentabilidade tem com a indústria têxtil?</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
 
-    <p class="p2">Seção 2 - Qual relação a sustentabilidade tem com a indústria têxtil?</p>
-
-    <p class="p2">Resposta: A indústria têxtil tem um grande impacto ambiental devido ao seu alto consumo de 
-        água e energia, contribuindo para a escassez de recursos naturais e as mudanças climáticas. 
+          <div class="content">
+            <p>
+            A indústria têxtil tem um grande impacto ambiental devido ao seu alto consumo de
+        água e energia, contribuindo para a escassez de recursos naturais e as mudanças climáticas.
         É crucial que as empresas adotem processos mais eficientes e fontes de energia limpa para reduzir esse
          impacto.
         Além disso, a produção de fibras sintéticas, como o poliéster, libera microplásticos durante a lavagem das
- roupas. Escolher fibras naturais e métodos de tingimento sustentáveis pode ajudar a mitigar esse problema e a
-  poluição dos oceanos.
-A indústria têxtil tem o potencial de contribuir para um futuro mais sustentável. Os consumidores desempenham 
-um papel importante ao escolher marcas comprometidas com a sustentabilidade e ao pressionar por mudanças 
-positivas. Juntos, podemos transformar essa indústria em uma força positiva para o planeta e as pessoas, pois 
-a moda consciente é o caminho para o futuro.</p>
-        
-   
+        roupas. Escolher fibras naturais e métodos de tingimento sustentáveis pode ajudar a mitigar esse problema e a
+        poluição dos oceanos.
+        A indústria têxtil tem o potencial de contribuir para um futuro mais sustentável. Os consumidores desempenham
+        um papel importante ao escolher marcas comprometidas com a sustentabilidade e ao pressionar por mudanças
+        positivas. Juntos, podemos transformar essa indústria em uma força positiva para o planeta e as pessoas, pois
+        a moda consciente é o caminho para o futuro.
+
+            </p>
+
+          </div>
         </div>
+
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>4. Esqueci minhas informações de cadastro/login. Como posso recupera-las?</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
+
+          <div class="content">
+            <p>
+            Para recuperar suas informações de cadastro/login, entre em contato com o suporte técnico para poder
+            recuperar ou redefinir estes dados.
+
+            </p>
+          </div>
+        </div>
+
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>5. Como entrar em contato com o suporte ao cliente?</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
+
+          <div class="content">
+            <p>
+            Você pode entrar em contato com o suporte a partir do rodapé da página principal do site, basta
+            identificar a seção de "Entre em contato" e então clicar no botão de "Contato" localizado logo 
+            abaixo desta seção.
+
+            </p>
+          </div>
+        </div>
+
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>6. O ALFI está disponível internacionalmente?</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
+
+          <div class="content">
+            <p>
+            No estado de desenvolvimento atual o ALFI pode ser acessado de qualquer dispositivo a qualquer momento,
+            mas as traduções estão apenas disponiveis em PT-BR.
+
+            </p>
+          </div>
+        </div>
+
+        <div class="content-container">
+          <div class="faq-header">
+            <h3>7. Outro...</h3>
+            <span class="open active">+</span>
+            <span class="close">-</span>
+          </div>
+
+          <div class="content">
+            <p>
+            Como já esclarecido no cabeçalho desta página, se sua dúvida não estiver dentre as citadas
+            entre em contato com o suporte para que possamos esclarecê-la. Fique atento que o suporte irá
+            entrar em contato no prazo máximo de uma semana.
+
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
+
+    <script>
+        const faqHeaders = document.querySelectorAll(".faqs-container .faq-header");
+
+faqHeaders.forEach((header, i) => {
+  header.addEventListener("click", () => {
+    header.nextElementSibling.classList.toggle("active");
+
+    const open = header.querySelector(".open");
+    const close = header.querySelector(".close");
+
+    if (header.nextElementSibling.classList.contains("active")) {
+      open.classList.remove("active");
+      close.classList.add("active");
+    } else {
+      open.classList.add("active");
+      close.classList.remove("active");
+    }
+  });
+});
+    </script>
     
     </body>
 </html>
